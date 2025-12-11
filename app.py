51b57a216c71cd16ac1b8228206e0694a29b5595
mennahmed3456-rgb <mennahmed3456@gmail.com>
2025-12-11 20:18:59 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# تحميل الموديل
+model = joblib.load("final_relapse_model.pkl")
+
+def score_to_class(score):
+    if score < 0.25:
+        return "Stable"
+    elif score < 0.55:
+        return "At_Risk"
+    else:
+        return "Relapsed"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    features = [
+        data["Academic_Performance_Decline"],
+        data["Social_Isolation"],
+        data["Financial_Issues"],
+        data["Physical_Mental_Health_Problems"],
+        data["Legal_Consequences"],
+        data["Relationship_Strain"],
+        data["Risk_Taking_Behavior"],
+        data["Withdrawal_Symptoms"],
+        data["Denial_and_Resistance_to_Treatment"]
+    ]
+
+    features = np.array(features).reshape(1, -1)
+
+    score = float(model.predict(features)[0])
+    category = score_to_class(score)
+
+    return jsonify({
+        "risk_score": round(score, 4),
+        "risk_category": category
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
